9b412c056a352b658a88a8ce15f37e047d6eaa15
Krishna Prabha L P <68377201+krishna-123-star@users.noreply.github.com>
2023-08-02 17:59:07 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,9 @@
+const joinArrayElements = (arr, separator) => arr.join(separator);
+
+const array = ["Krishna", "Prabha", ""];
+const separator = " ";
+
+const result = joinArrayElements(array, separator);
+console.log(result); 
+
+/* Krishna Prabha */
